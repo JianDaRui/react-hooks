c87c898c58c428ae8e493ece76e14f3d1664f8c7
jiandarui <jiandadarui@gmail.com>
2022-12-18 09:11:54 +0800

    useEffect

--- src/pages/use-state/index.jsx
+++ src/pages/use-state/index.jsx
@@ -23,7 +23,7 @@
 
 // export default UseStateFeat
 
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect, useLayoutEffect } from "react";
 
 function getInitialValue() {
   console.log('getInitialValue is getting executed');
@@ -31,21 +31,62 @@ function getInitialValue() {
   return 0;
 }
 
-function Counter() {
-  const [count, setCount] = useState(() => getInitialValue());
-  const ref = useRef(0)
-  function increment() {
-    ref.current += 1
-    // setCount(count + 1);
-  }
+// function Counter() {
+//   const [count, setCount] = useState(() => getInitialValue());
+//   const ref = useRef(0)
+//   function increment() {
+//     ref.current += 1
+//     // setCount(count + 1);
+//   }
 
-  const random = Math.floor(Math.random()* 10000000000)
+//   const random = Math.floor(Math.random() * 10000000000)
+
+//   return (
+//     <div>
+//       <h1 style={{ color: 'pink' }}>{random}</h1>
+//       <button onClick={increment}>{ref.current}</button>
+//     </div>
+//   )
+// }
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    if (count === 0) {
+      const randomNum = 10 + Math.random() * 200
+      setCount(Math.floor(Math.random() * 10000000000));
+    }
+  }, [count]);
+
+  // useLayoutEffect(() => {
+  //   if (count === 0) {
+  //     const randomNum = 10 + Math.random()*200
+  //     setCount(Math.floor(Math.random() * 10000000000));
+  //   }
+  // }, [count]);
+
+  return (
+    <div onClick={() => setCount(0)}>{count}</div>
+  );
+}
+
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => {
+      console.log(`You clicked ${count} times`);
+    }, 3000);
+  }, [count]);
 
   return (
     <div>
-      <h1 style={{color: 'pink'}}>{random}</h1>
-      <button onClick={increment}>{ref.current}</button>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
     </div>
-  )
+  );
 }
 export default Counter
